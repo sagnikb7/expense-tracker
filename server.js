762747fd37a64913bd2aab13330b2cc809179aac
sagnik <sagnik.betal@campk12.com>
2022-05-24 20:58:45 +0530

    trying module alias

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const { NODE_ENV } = process.env;
 require('dotenv').config({ path: `${__dirname}/${NODE_ENV}.env` });
+require('module-alias/register');
+
+const logger = require('./src/core/logger')('server');
 
 const app = require('./src/app');
 const config = require('./src/config/app');
-
-const logger = require('./src/core/logger')('server');
 
 app.listen(config.NODE_PORT, () => {
   logger.info(`server started on PORT:${config.NODE_PORT}`);
